765a4bf2d5c047c0d348d85a05a082cc17d60e58
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2019-11-02 20:27:03 -0500

    switching

--- src/components/costandpayment/coststream.js
+++ src/components/costandpayment/coststream.js
@@ -1,46 +1,46 @@
-import React, { useRef, useEffect, useState } from "react";
-import AddCostForm from "./costform"
-import ApiManger from "../../modules/APIManager"
+// import React, { useRef, useEffect, useState } from "react";
+// import AddCostForm from "./costform"
+// import ApiManger from "../../modules/APIManager"
 
 
-const CostStream = props => {
+// const CostStream = props => {
 
-const [costData, setCost] = useState([]);
+// const [costData, setCost] = useState([]);
 
-const getCost = () => {
-    return ApiManger.chartdataAll( props.match.params.collegeId).then(setCost)
-}
-   useEffect(() => {
-    getCost();
+// const getCost = () => {
+//     return ApiManger.chartdataAll( props.match.params.collegeId).then(setCost)
+// }
+//    useEffect(() => {
+//     getCost();
 
-  }, []);
+//   }, []);
 
-  {costData.map(cost=> {
-
-
-return (
-        <>
-          <h1>College Cost </h1>
-          <div>
-          <AddCostForm {...props} />
-          </div>
-             return (
-
-              <div key={cost.year} className="card">
-                  <h3>{cost.name}</h3>
-                <ul>
-                  <li> college ${}</li>
-                  <br />
-                </ul>
-              </div>
-
-               )
+//   {costData.map(cost=> {
 
 
-        </>
-          )
-  })
-        }
-    }
-export default CostStream;
+// return (
+//         <>
+//           <h1>College Cost </h1>
+//           <div>
+//           <AddCostForm {...props} />
+//           </div>
+//              return (
+
+//               <div key={cost.year} className="card">
+//                   <h3>{cost.name}</h3>
+//                 <ul>
+//                   <li> college ${}</li>
+//                   <br />
+//                 </ul>
+//               </div>
+
+//                )
+
+
+//         </>
+//           )
+//   })
+//         }
+//     }
+// export default CostStream;
 
